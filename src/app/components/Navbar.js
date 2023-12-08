3a85361f815927b8efee99e9df2858a5c584c6e5
Harshul Jain <harshulgangwal02@gmail.com>
2023-12-08 11:21:22 +0530

    Added Navbar

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,14 @@
+
+const Navbar = () => {
+  return (
+    
+
+<nav class="bg-blue-500 border-gray-200 dark:bg-gray-900">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 text-white font-medium text-xl">
+    CanvasHome
+  </div>
+</nav>
+  )
+}
+
+export default Navbar
